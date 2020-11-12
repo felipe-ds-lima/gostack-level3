8d42fab5e6bca7bba4c21bc9039da1a6fc14f27f
Felipe D S Lima <felipe@monts.com.br>
2020-11-12 01:16:08 -0300

    primeiro projeto react

---
+++ primeiro-projeto-react/src/styles/global.ts
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+import githubBackground from '../assets/github-background.svg';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+    background-color: #f0f0f5;
+    background-image: url(${githubBackground});
+    background-repeat: no-repeat;
+    background-position: 70% top;
+  }
+
+  body, input, button {
+    font: 16px Roboto, sans-serif;
+  }
+
+  #root {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 40px 20px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
